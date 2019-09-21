39c95fcf267ab2af42280eeb00e10d6ef17ebfa1
cabreraem1 <52495686+cabreraem1@users.noreply.github.com>
2019-09-20 22:37:38 -0400

    Add project files.

---
+++ GradingSystem/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int grade = 0;
+
+            Console.WriteLine("Please enter the student's Grade");
+            grade = Convert.ToInt32(Console.ReadLine());
+
+            if (grade >=90)
+            {
+                Console.WriteLine("Your grade is an A!");
+            }
+            else if (grade < 90 && grade >= 80)
+            {
+                Console.WriteLine("Your grade is a B!");
+            }
+            else if (grade < 80 && grade >= 70)
+            {
+                Console.WriteLine("Your grade is a C!");
+            }
+            else if (grade < 70 && grade >= 60)
+            {
+                Console.WriteLine("Your grade is a D!");
+            }
+            else
+            {
+                Console.WriteLine("Your grade is an F");
+            }
+            Console.ReadLine();
+        }
+    }
+}
